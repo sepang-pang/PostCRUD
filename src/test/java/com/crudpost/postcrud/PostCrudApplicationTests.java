1616373623e69753e2fc00a160ce03b913be4d63
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-06-15 03:12:48 +0900

    <feat> : init commit

---
+++ src/test/java/com/crudpost/postcrud/PostCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudpost.postcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
